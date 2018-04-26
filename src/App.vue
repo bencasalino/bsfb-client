2c66fe776bc51d8857c56a89dbba7400191c71e4
Ben Casalino <bencasalino@gmail.com>
2018-04-26 09:23:20 -0600

    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <!-- <Header/> -->
-        <Test/>
+        <Header/>
+        <Subheader/>
         <div class="split">
           <Aside/>
           <Main/>
         </div>
-
         <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
-import Test from '@/components/Test'
+import Subheader from '@/components/Subheader'
 import Main from '@/components/Main'
 import Aside from '@/components/Aside'
 
@@ -22,7 +21,7 @@ export default {
   name: 'App',
   components: {
     Header,
-    Test,
+    Subheader,
     Main,
     Aside
   }
@@ -32,7 +31,7 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css?family=Fira+Sans|Nanum+Gothic:700');
 #app {
-    background-color:#f7f8f3 !important;
+    background-color:#f7f8f3;
   color: #222;
       font-family: 'Fira Sans', sans-serif;
       font-weight: 400;
---
+++ src/components/Subheader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+ldkfjskdf
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Subheader",
+    data() {
+      return {
+      };
+    }
+  };
+</script>
+
+
+<style scoped>
+
+</style>
